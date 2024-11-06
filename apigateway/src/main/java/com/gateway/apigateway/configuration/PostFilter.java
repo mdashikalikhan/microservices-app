faa38f359b82de1bbb13751c99f9c5caaac8b302
mdashikalikhan <khan.ashik@gmail.com>
2024-11-06 13:56:29 +0600

    Post Filter to APi Gateway

---
+++ apigateway/src/main/java/com/gateway/apigateway/configuration/PostFilter.java
@@ -0,0 +1,20 @@
+package com.gateway.apigateway.configuration;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component @Slf4j
+public class PostFilter implements GlobalFilter {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        return chain.filter(exchange).then(Mono.fromRunnable(
+                ()->{
+                    log.info("Post filter executed.....");
+                }
+        ));
+    }
+}
