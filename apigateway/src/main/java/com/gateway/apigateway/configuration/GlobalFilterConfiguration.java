7c9779cedc17bc381e415eeec14b892c26548fd3
mdashikalikhan <khan.ashik@gmail.com>
2024-11-07 14:14:57 +0600

    Global pre and post filter added

--- apigateway/src/main/java/com/gateway/apigateway/configuration/GlobalFilterConfiguration.java
+++ apigateway/src/main/java/com/gateway/apigateway/configuration/GlobalFilterConfiguration.java
@@ -2,16 +2,47 @@ package com.gateway.apigateway.configuration;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
 
 @Component
 @Slf4j
 public class GlobalFilterConfiguration {
 
-    public GlobalFilter filter() {
+    @Bean
+    public GlobalFilter secondFilter() {
         return (exchange, chain)->{
-            log.info("Global pre filter executed");
-            return chain.filter(exchange);
+            log.info("Global second pre filter executed");
+            return chain.filter(exchange).then(Mono.fromRunnable(
+                    ()->{
+                        log.info("Global second post filter executed...");
+                    }
+            ));
+        };
+    }
+
+    @Bean
+    public GlobalFilter thirdFilter() {
+        return (exchange, chain)->{
+            log.info("Global third pre filter executed");
+            return chain.filter(exchange).then(Mono.fromRunnable(
+                    ()->{
+                        log.info("Global third post filter executed...");
+                    }
+            ));
+        };
+    }
+
+    @Bean
+    public GlobalFilter fourthFilter() {
+        return (exchange, chain)->{
+            log.info("Global fourth pre filter executed");
+            return chain.filter(exchange).then(Mono.fromRunnable(
+                    ()->{
+                        log.info("Global fourth post filter executed...");
+                    }
+            ));
         };
     }
 }
