dca73d65a331cc1d7a347cdcb494d911d1752a44
mdashikalikhan <khan.ashik@gmail.com>
2024-12-15 18:12:40 +0600

    album list controller

--- album/src/main/java/com/photo/album/controllers/AlbumController.java
+++ album/src/main/java/com/photo/album/controllers/AlbumController.java
@@ -1,17 +1,22 @@
 package com.photo.album.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.photo.album.entity.AlbumEntity;
 import com.photo.album.model.ui.AlbumResponseModel;
 import com.photo.album.service.AlbumService;
 import jakarta.ws.rs.Produces;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController("/albums")
 @Slf4j
@@ -26,9 +31,12 @@ public class AlbumController {
             MediaType.APPLICATION_JSON_VALUE,
             MediaType.APPLICATION_XML_VALUE
     })
-
     public List<AlbumResponseModel> getUserAlbums(@PathVariable String id){
-        albumService.getAlbums(id).
+        List<AlbumEntity> albumEntities = Optional.ofNullable(albumService.getAlbums(id)).filter(album -> !album.isEmpty())
+                .orElse(new ArrayList<>());
+        ModelMapper modelMapper = new ModelMapper();
+        List<AlbumResponseModel> albumResponseModels = albumEntities.stream().map(album -> modelMapper.map(album, AlbumResponseModel.class)).toList();
+        return albumResponseModels;
     }
 
 }
