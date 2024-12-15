5def3f190f12bc0f1fa813151a642ad7198f2780
Md. Ashik Ali Khan <khan.ashik@gmail.com>
2024-12-15 09:47:56 +0600

    cc

--- album/src/main/java/com/photo/album/controllers/AlbumController.java
+++ album/src/main/java/com/photo/album/controllers/AlbumController.java
@@ -2,9 +2,11 @@ package com.photo.album.controllers;
 
 import com.photo.album.model.ui.AlbumResponseModel;
 import com.photo.album.service.AlbumService;
+import jakarta.ws.rs.Produces;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,9 +21,14 @@ public class AlbumController {
     @NonNull
     private AlbumService albumService;
 
-    @GetMapping("/user/{id}")
-    public List<AlbumResponseModel> getUserAlbums(@PathVariable String id){
+    @GetMapping(path="/user/{id}",
+    produces = {
+            MediaType.APPLICATION_JSON_VALUE,
+            MediaType.APPLICATION_XML_VALUE
+    })
 
+    public List<AlbumResponseModel> getUserAlbums(@PathVariable String id){
+        albumService.getAlbums(id).
     }
 
 }
--- album/src/main/java/com/photo/album/entity/AlbumEntity.java
+++ album/src/main/java/com/photo/album/entity/AlbumEntity.java
@@ -1,10 +1,15 @@
 package com.photo.album.entity;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
 public class AlbumEntity {
 
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private long id;
     private String albumId;
     private String albumName;
