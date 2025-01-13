5eaeafd527c8d6639a0f650c70271e3a60234e6f
mdashikalikhan <khan.ashik@gmail.com>
2025-01-13 18:05:42 +0600

    Resilience continued...

--- user-service/src/main/java/com/gateway/user_service/service/AlbumServiceClient.java
+++ user-service/src/main/java/com/gateway/user_service/service/AlbumServiceClient.java
@@ -1,17 +1,26 @@
 package com.gateway.user_service.service;
 
 import com.gateway.user_service.model.AlbumResponseModel;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @FeignClient (value = "album-service", path = "/albums")
 public interface AlbumServiceClient {
 
     @GetMapping("/user/{id}")
+    @CircuitBreaker(name = "album-service", fallbackMethod = "getAlbumsFallback")
     public ResponseEntity<List<AlbumResponseModel>> getAlbums(@PathVariable("id") String id);
+
+    default ResponseEntity<List<AlbumResponseModel>> getAlbumsFallback(String id,
+                                                                       Throwable exception){
+        System.out.println("fallback: " + id);
+        return ResponseEntity.internalServerError().body(new ArrayList<>());
+    }
 
 }
